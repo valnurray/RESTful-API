1e485c2750ffbabc057fe06a8cca643adbf7c087
valnurray <valnurray@gmail.com>
2021-06-22 16:05:13 +0300

    initital and creating a project skeleton

---
+++ src/main/java/com/lankin/rest/RestNumberTwoApplication.java
@@ -0,0 +1,13 @@
+package com.lankin.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNumberTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestNumberTwoApplication.class, args);
+    }
+
+}
