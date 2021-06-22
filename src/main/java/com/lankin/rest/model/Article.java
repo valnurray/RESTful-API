9b8f6ae9b1054bf19c4ffa405464e58c480b03cf
valnurray <valnurray@gmail.com>
2021-06-22 18:00:20 +0300

    Create Article JPA Entity

---
+++ src/main/java/com/lankin/rest/model/Article.java
@@ -0,0 +1,30 @@
+package com.lankin.rest.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+
+
+@Data
+@Entity
+@Table(name = "article")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "body")
+    private String body;
+}
