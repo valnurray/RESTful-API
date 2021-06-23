19356fe39915da21142f316ae753a2c6f9db02db
valnurray <valnurray@gmail.com>
2021-06-23 10:47:27 +0300

    Create service structure

---
+++ src/main/java/com/lankin/rest/service/ArticleService.java
@@ -0,0 +1,4 @@
+package com.lankin.rest.service;
+
+public interface ArticleService {
+}
---
+++ src/main/java/com/lankin/rest/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,6 @@
+package com.lankin.rest.service.impl;
+
+import com.lankin.rest.service.ArticleService;
+
+public class ArticleServiceImpl implements ArticleService {
+}
