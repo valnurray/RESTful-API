655236e8d2289652f2ecf4a51a9d22aeedf85673
valnurray <valnurray@gmail.com>
2021-06-23 10:53:17 +0300

    add @Service annotation to ArticleServiceImpl

--- src/main/java/com/lankin/rest/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/lankin/rest/service/impl/ArticleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.lankin.rest.service.impl;
 
 import com.lankin.rest.service.ArticleService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ArticleServiceImpl implements ArticleService {
 }
