8d29528040fa0447477ff64b94e5b49513e407a1
valnurray <valnurray@gmail.com>
2021-06-30 17:11:18 +0300

    added swagger documentation plugin and link

--- src/main/java/com/lankin/rest/RestNumberTwoApplication.java
+++ src/main/java/com/lankin/rest/RestNumberTwoApplication.java
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  *
  */
 @SpringBootApplication
+/*http://localhost:8080/v2/api-docs*/
 @EnableSwagger2
 public class RestNumberTwoApplication {
 
